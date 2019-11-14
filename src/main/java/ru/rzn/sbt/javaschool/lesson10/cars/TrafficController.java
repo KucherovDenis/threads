4b918789a651df077f9e277f7fd3c942559c9f7d
KucherovDenis <denku-87@mail.ru>
2019-11-14 15:58:57 +0300

    Потоки

--- src/main/java/ru/rzn/sbt/javaschool/lesson10/cars/TrafficController.java
+++ src/main/java/ru/rzn/sbt/javaschool/lesson10/cars/TrafficController.java
@@ -1,15 +1,38 @@
 package ru.rzn.sbt.javaschool.lesson10.cars;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TrafficController {
+
+    private volatile AtomicInteger countLeft = new AtomicInteger(0);
+
+    private volatile AtomicInteger countRight = new AtomicInteger(0);
+
+    private void waitTime() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void enterLeft() {
+        while (countRight.get() != 0) {
+        }
+        countLeft.incrementAndGet();
     }
 
     public void enterRight() {
+        while (countLeft.get() != 0) {
+        }
+        countRight.incrementAndGet();
     }
 
     public void leaveLeft() {
+        countLeft.decrementAndGet();
     }
 
     public void leaveRight() {
+        countRight.decrementAndGet();
     }
 }
